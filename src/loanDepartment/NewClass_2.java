ac494bc5870d8d18bdbb17ac878d07bce098a154
KishoreGopi5577 <gopikishore5577@gmail.com>
2024-03-28 22:39:39 +0530

    Third Commit in Kishore branch

--- src/loanDepartment/NewClass_2.java
+++ src/loanDepartment/NewClass_2.java
@@ -2,11 +2,11 @@ package loanDepartment;
 
 import org.testng.annotations.Test;
 
-public class NewClass_1 {
+public class NewClass_2 {
 	
 	@Test
 	public void newClass1() {
-		System.out.println("Newly added class 1");
+		System.out.println("Newly added class 2");
 	}
 
 }
