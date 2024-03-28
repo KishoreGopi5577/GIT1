1698696056dc6d236c1377ad377643c681680551
KishoreGopi5577 <gopikishore5577@gmail.com>
2024-03-28 22:37:23 +0530

    Second Commit

---
+++ src/loanDepartment/NewClass_1.java
@@ -0,0 +1,12 @@
+package loanDepartment;
+
+import org.testng.annotations.Test;
+
+public class NewClass_1 {
+	
+	@Test
+	public void newClass1() {
+		System.out.println("Newly added class");
+	}
+
+}
