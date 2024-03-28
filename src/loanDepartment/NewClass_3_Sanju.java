6b7712fef3e3b15a8f2cfbdf4b78046c50eb5621
KishoreGopi5577 <gopikishore5577@gmail.com>
2024-03-28 22:45:49 +0530

    Commit from Sanju branch

---
+++ src/loanDepartment/NewClass_3_Sanju.java
@@ -0,0 +1,12 @@
+package loanDepartment;
+
+import org.testng.annotations.Test;
+
+public class NewClass_3_Sanju {
+	
+	@Test
+	public void newSanjuClass() {
+		System.out.println("New Sanju class");
+	}
+
+}
